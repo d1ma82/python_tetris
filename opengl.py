b9056ffd7657bac50533e2ede6b38c0ac7322648
Dima Zv <dimazverincevchanel@gmail.com>
2023-10-24 21:51:34 +0300

    continue opengl

--- opengl.py
+++ opengl.py
@@ -6,22 +6,37 @@ class Programm:
 
     def __init__(self, fragment, vertex) -> None:
         
+        self.array_object   = ogl.glGenVertexArrays(1)
+        self.vertex_buffer  = ogl.glGenBuffers(1)       
+        self.element_buffer = ogl.glGenBuffers(1)
+    
         self.texture          : ogl.GLuint = 0
         self.vertex_shader    : ogl.GLuint = create_shader(vertex, ogl.GL_VERTEX_SHADER)
         self.fragment_shader  : ogl.GLuint = create_shader(fragment, ogl.GL_FRAGMENT_SHADER)
         
-        if self.vertex_shader == 0 or self.fragment_shader == 0 : 
-            print('Could not create program')
-            return
+        if self.vertex_shader == 0 or self.fragment_shader == 0 : return
         
-        self.id               : ogl.GLuint = create_programm(self.vertex_shader, self.fragment_shader)
-        self.VAO              : ogl.GLuint = 0         # Vertex Array object
-        self.VBO              : ogl.GLuint = 0         # Vertex Buffer Object
-        self.EBO              : ogl.GLuint = 0         # Element Buffer Object
-
+        self.id : ogl.GLuint = create_programm(self.vertex_shader, self.fragment_shader)
+        
         pass
 
     def __del__(self) -> None:
+
+        if self.texture > 0: ogl.glDeleteTextures(len(self.texture), self.texture)
+        if self.fragment_shader > 0: 
+            ogl.glDetachShader(self.id, self.fragment_shader)
+            ogl.glDeleteShader(self.fragment_shader)
+        
+        if self.vertex_shader > 0: 
+            ogl.glDetachShader(self.id, self.vertex_shader)
+            ogl.glDeleteShader(self.vertex_shader)
+
+        if self.id > 0: ogl.glDeleteProgram(self.id)
+
+        ogl.glDeleteBuffers(1, self.element_buffer)
+        ogl.glDeleteBuffers(1, self.vertex_buffer)
+        ogl.glDeleteVertexArrays(1, self.array_object)
+
         pass
 
 
@@ -30,9 +45,8 @@ def create_shader(file, type) -> ogl.GLuint:
     src: asset.Source = asset.Source(file)
     if not src.is_opened(): return 0
 
-    shader : ogl.GLuint = ogl.glCreateShader(type)
-    code = src.read()
-    ogl.glShaderSource(shader, code)
+    shader = ogl.glCreateShader(type)
+    ogl.glShaderSource(shader, src.read())
     print('Compile shader')
     ogl.glCompileShader(shader)
 
@@ -64,4 +78,17 @@ def create_programm(vertex_shader, fragment_shader)->ogl.GLuint:
         prog=0
     else: print('Link ok')
     return prog
+
+def d2_texture(width, height, bytes)-> ogl.GLuint:
+    
+    tex = ct.c_int(0)
+    ogl.glGenTextures(1, ct.byref(tex))
+    ogl.glBindTexture(ogl.GL_TEXTURE_2D, tex)
+    ogl.glTexParameteri(ogl.GL_TEXTURE_2D, ogl.GL_TEXTURE_WRAP_S, ogl.GL_CLAMP_TO_EDGE)
+    ogl.glTexParameteri(ogl.GL_TEXTURE_2D, ogl.GL_TEXTURE_WRAP_T, ogl.GL_CLAMP_TO_EDGE)
+    ogl.glTexParameteri(ogl.GL_TEXTURE_2D, ogl.GL_TEXTURE_MIN_FILTER, ogl.GL_LINEAR)
+    ogl.glTexParameteri(ogl.GL_TEXTURE_2D, ogl.GL_TEXTURE_MAG_FILTER, ogl.GL_LINEAR)
+    ogl.glTexImage2D(ogl.GL_TEXTURE_2D, 0, ogl.GL_RGB, 
+                     width, height, 0, ogl.GL_RGB, ogl.GL_UNSIGNED_BYTE, bytes)
+    return tex
     
